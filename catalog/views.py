3c15a7035fdc5446d7787142ca5db7d5d6789c66
Itschik <155478606+Itschik@users.noreply.github.com>
2025-08-26 10:36:10 +0200

    Update views.py

--- catalog/views.py
+++ catalog/views.py
@@ -8,7 +8,3 @@ def home(request):
 def product_detail(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     return render(request, 'catalog/product_detail.html', {'product': product})
-
-def index(request):
-    return render(request, "catalog/index.html")
-
